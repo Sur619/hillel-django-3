0ef29488d68194b1bbb02ac87c3ccc9b7a11b8dd
Sur619 <whatyoulookingfor619@gmail.com>
2024-06-17 22:33:18 +0300

    add receiver for model order

--- products/models/order.py
+++ products/models/order.py
@@ -1,8 +1,12 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from products.models import Product
 
+from telegram.client import send_message
 
 class Order(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
@@ -18,3 +22,11 @@ class Order(models.Model):
         for order_product in self.order_products.all():
             total_price += order_product.price
         return total_price
+
+
+@receiver(post_save, sender=Order)
+def send_order_telegram_message(sender, instance, created, **kwargs):
+    if created:
+        chat_id = 980106016
+        text = f"new order {instance.uuid} created"
+        send_message(chat_id, text)
---
+++ telegram/client.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.environ['TELEGRAM_BOT_TOKEN']
+
+
+def send_message(chat_id, text):
+    telegram_url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    data = {
+        'chat_id' : chat_id,
+        'text' : text
+    }
+    response = requests.post(telegram_url, data=data)
+    print(response.json())
+
+if __name__ == '__main__':
+    chat_id = os.environ['TELEGRAM_BOT_CHAT_ID']
+    send_message(chat_id, 'heyyyyy')
